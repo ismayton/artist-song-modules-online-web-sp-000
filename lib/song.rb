a55de496814fe30fc63530483c1d391a459d0103
Ian Mayton <ian.mayton@gmail.com>
2020-07-28 18:47:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require 'pry'
 class Song
   
   extend Memorable 
-
+  extend Findable
+  
   attr_accessor :name
   attr_reader :artist
 
@@ -13,9 +14,7 @@ class Song
     @@songs << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  
 
   def self.all 
     @@songs 
